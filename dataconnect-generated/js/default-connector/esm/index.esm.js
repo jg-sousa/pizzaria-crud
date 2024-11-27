343f87323944f89e02e71ba7e46bcd4270dc38b4
João Sousa <jgsoaressousa772@outlook.com>
2024-11-27 16:06:47 -0300

    firebase att

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'pizzaria-crud',
+  location: 'us-central1'
+};
+
